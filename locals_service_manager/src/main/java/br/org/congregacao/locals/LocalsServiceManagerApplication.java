f2417a38b3160002214410cb12faa6b40e0e4ef2
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-10-19 17:02:41 -0300

    created first locals service manager version

---
+++ locals_service_manager/src/main/java/br/org/congregacao/locals/LocalsServiceManagerApplication.java
@@ -0,0 +1,13 @@
+package br.org.congregacao.locals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalsServiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalsServiceManagerApplication.class, args);
+	}
+
+}

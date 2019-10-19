818b4fbf356f10cfd830a7d3b74aee3c320afc2e
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-10-19 19:35:55 -0300

    created administration and regional repository and service

---
+++ locals_service_manager/src/main/java/br/org/congregacao/locals/service/RegionalService.java
@@ -0,0 +1,8 @@
+package br.org.congregacao.locals.service;
+
+import br.org.congregacao.locals.repository.RegionalRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface RegionalService extends RegionalRepository {
+}

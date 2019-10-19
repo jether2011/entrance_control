d6e179d2a8947a0768a6f183a6a8a6eb3a7c9c72
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-10-19 19:58:13 -0300

    created administration resource

--- locals_service_manager/src/main/java/br/org/congregacao/locals/application/resources/AdministrationResource.java
+++ locals_service_manager/src/main/java/br/org/congregacao/locals/application/resources/AdministrationResource.java
@@ -5,16 +5,14 @@ import br.org.congregacao.locals.domain.Administration;
 import br.org.congregacao.locals.service.AdministrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.io.Serializable;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/administrations")
@@ -23,8 +21,19 @@ public class AdministrationResource implements Serializable {
     @Autowired
     private AdministrationService administrationService;
 
+    @GetMapping
     public ResponseEntity<List<Administration>> getAll() {
         return ResponseEntity.ok().body(administrationService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Administration> getOne(@PathVariable final String id) {
+        final Optional<Administration> optionalAdministration = administrationService.findById(id);
+
+        if (optionalAdministration.isPresent())
+            return ResponseEntity.ok().body(optionalAdministration.get());
+        else
+            return ResponseEntity.notFound().build();
     }
 
     @PostMapping
@@ -32,7 +41,7 @@ public class AdministrationResource implements Serializable {
         final Administration administration = AdministrationRequest.from(request);
         administrationService.save(administration);
 
-        final URI uri = uriBuilder.path("/administrations/{id}").buildAndExpand(administration.getId()).toUri();
+        final URI uri = uriBuilder.path("/api/v1/administrations/{id}").buildAndExpand(administration.getId()).toUri();
         return ResponseEntity.created(uri).body(administration);
     }
 

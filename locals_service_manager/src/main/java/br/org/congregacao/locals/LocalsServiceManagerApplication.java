afc0080bf6655c6abd514807f800fd1ec4150a64
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-10-19 18:38:53 -0300

    added configurations at application main class

--- locals_service_manager/src/main/java/br/org/congregacao/locals/LocalsServiceManagerApplication.java
+++ locals_service_manager/src/main/java/br/org/congregacao/locals/LocalsServiceManagerApplication.java
@@ -1,9 +1,15 @@
 package br.org.congregacao.locals;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "br.org.congregacao.locals")
+@EntityScan("br.org.congregacao.locals.domain")
 public class LocalsServiceManagerApplication {
 
 	public static void main(String[] args) {

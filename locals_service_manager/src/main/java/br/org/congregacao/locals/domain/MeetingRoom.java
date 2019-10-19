e2e49263ac4418af257a4b8282d4c4fa09678892
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-10-19 18:57:13 -0300

    crated meeting room document class

---
+++ locals_service_manager/src/main/java/br/org/congregacao/locals/domain/MeetingRoom.java
@@ -0,0 +1,82 @@
+package br.org.congregacao.locals.domain;
+
+import br.org.congregacao.locals.domain.types.EntranceType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.azam.ulidj.ULID;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Document(collection = "meeting_room")
+public class MeetingRoom implements Serializable {
+
+    @Id
+    private String id = ULID.random();
+    @Indexed(unique = true)
+    private String name;
+    private String description;
+    @Indexed
+    private Set<EntranceType> entrances = new HashSet<>();
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updated;
+
+    public MeetingRoom() {
+        this.created = LocalDateTime.now();
+        this.updated = LocalDateTime.now();
+    }
+
+    private MeetingRoom(final String name, final String description, final String entrance) {
+        this.name = name;
+        this.description = description;
+        this.created = LocalDateTime.now();
+        this.updated = LocalDateTime.now();
+
+        this.addEntrance(entrance);
+    }
+
+    public static MeetingRoom of(final String name, final String description, final String entrance) {
+        return new MeetingRoom(name, description, entrance);
+    }
+
+    public void addEntrance(final String entrance) {
+        this.entrances.add(Objects.requireNonNull(EntranceType.valueOf(entrance), "The Entrance Value was not found!"));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Set<EntranceType> getEntrances() {
+        return Collections.unmodifiableSet(entrances);
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(final LocalDateTime updated) {
+        this.updated = updated;
+    }
+}

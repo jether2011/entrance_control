70de7e36f00d3c35c192dea196619c850045cce8
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2019-11-11 20:24:14 -0200

    updated entrance type

--- locals_service_manager/src/main/java/br/org/congregacao/locals/domain/types/EntranceType.java
+++ locals_service_manager/src/main/java/br/org/congregacao/locals/domain/types/EntranceType.java
@@ -2,6 +2,16 @@ package br.org.congregacao.locals.domain.types;
 
 public enum EntranceType {
 
-    MAIN_ENTRANCE
+    MAIN_ENTRANCE("Entrada Principal");
+	
+	private final String type;
+	
+	private EntranceType(final String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
 
 }

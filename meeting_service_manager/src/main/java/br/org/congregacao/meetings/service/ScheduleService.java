43d955d6c48e248b3951de1e25830048fdd94c67
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2019-12-14 11:31:04 -0200

    remove unused schedule class, resource, service, repository and request dto

--- meeting_service_manager/src/main/java/br/org/congregacao/meetings/service/ScheduleService.java
+++
@@ -1,8 +0,0 @@
-package br.org.congregacao.meetings.service;
-
-import br.org.congregacao.meetings.repository.ScheduleRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ScheduleService extends ScheduleRepository {
-}

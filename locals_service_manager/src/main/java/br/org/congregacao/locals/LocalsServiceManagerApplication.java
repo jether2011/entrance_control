153f2ae880096780990db4e400804b1379791396
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2020-02-09 11:25:53 -0200

    general updates

--- locals_service_manager/src/main/java/br/org/congregacao/locals/LocalsServiceManagerApplication.java
+++ locals_service_manager/src/main/java/br/org/congregacao/locals/LocalsServiceManagerApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "br.org.congregacao.locals")
 @EntityScan("br.org.congregacao.locals.domain")
+@EnableDiscoveryClient
 public class LocalsServiceManagerApplication {
 
 	public static void main(String[] args) {

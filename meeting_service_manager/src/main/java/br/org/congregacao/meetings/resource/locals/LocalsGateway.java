884bf59236c659e59271904cb3736ac196efa752
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2020-03-31 00:13:35 -0300

    added feign integrations with church and your meeting rooms and workgroups

--- meeting_service_manager/src/main/java/br/org/congregacao/meetings/resource/locals/LocalsGateway.java
+++ meeting_service_manager/src/main/java/br/org/congregacao/meetings/resource/locals/LocalsGateway.java
@@ -1,15 +1,25 @@
 package br.org.congregacao.meetings.resource.locals;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import br.org.congregacao.meetings.resource.locals.response.AdministrationResponse;
+import br.org.congregacao.meetings.resource.locals.response.ChurchMeetingRoomsResponse;
+import br.org.congregacao.meetings.resource.locals.response.WorkGroupResponse;
 
 @FeignClient("locals")
 public interface LocalsGateway {
 
 	@GetMapping("/api/v1/administrations/{id}")
 	AdministrationResponse findAdministrationById(@PathVariable("id") final String id);
+	
+	@GetMapping("/api/v1/churches/{id}/meeting-rooms")
+	ChurchMeetingRoomsResponse findMeetingRoomsByChurchId(@PathVariable final String id);
+	
+	@GetMapping(produces = "${workgroups.api.version:application/vnd.workgroups.v1+json}")
+	List<WorkGroupResponse> findAllWorkGroups();
 	
 }
